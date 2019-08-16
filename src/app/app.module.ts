30062aecfb14c4f04dcee4e079cbdf5a3cadb09f
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-08-16 17:58:59 +1000

    The App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+/* Angular Modules */
+import { NgModule }                from '@angular/core';
+import { FormsModule }             from '@angular/forms';
+import { RouterModule }            from '@angular/router';
+import { BrowserModule }           from '@angular/platform-browser';
+import { HttpClientModule }        from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+/* Third-party Modules */
+import { ModalModule }             from 'ngx-bootstrap/modal';
+
+/* Routing Module */
+import { AppRoutingModule }        from './app-routing.module';
+
+import { StoreDevtoolsModule }     from '@ngrx/store-devtools';
+import { environment }             from 'src/environments/environment';
+
+/* App Modules */
+import { EventsModule }            from './events/events.module';
+
+/* App Component */
+import { AppComponent }            from './app.component';
 
 @NgModule({
   declarations: [
@@ -10,6 +27,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    EventsModule,
+    StoreDevtoolsModule.instrument(),
     AppRoutingModule
   ],
   providers: [],
