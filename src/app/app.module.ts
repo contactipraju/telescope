69ae4bc5da805d68d26d7c994ac79c9f3443993f
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-08-20 21:27:16 +1000

    Removing tile-view-episodes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -62,7 +62,6 @@ import { EditSeriesComponent }        from './shared/edit-series/edit-series.com
 import { EditEpisodeComponent }       from './shared/edit-episode/edit-episode.component';
 
 import { ListViewSeriesComponent }    from './shared/list-view-series/list-view-series.component';
-import { TileViewEpisodesComponent }  from './shared/tile-view-episodes/tile-view-episodes.component';
 
 import { TableViewPeopleComponent }   from './shared/table-view-people/table-view-people.component';
 import { TableViewSeriesComponent }   from './shared/table-view-series/table-view-series.component';
@@ -84,7 +83,6 @@ import { TableViewEpisodesComponent } from './shared/table-view-episodes/table-v
     EditSeriesComponent,
     EditEpisodeComponent,
     ListViewSeriesComponent,
-    TileViewEpisodesComponent,
     TableViewPeopleComponent,
     TableViewSeriesComponent,
     TableViewEpisodesComponent,
