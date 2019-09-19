98b966a2e5834129bb18d24c48f524c131a807dd
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-09-19 16:33:39 +1000

    small fix & deployment test

--- src/app/components/views-table/table-view-series/table-view-series.component.spec.ts
+++ src/app/components/views-table/table-view-series/table-view-series.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TableViewSeriesComponent } from './table-view.component';
+import { TableViewSeriesComponent } from './table-view-series.component';
 
 describe('TableViewSeriesComponent', () => {
   let component: TableViewSeriesComponent;
