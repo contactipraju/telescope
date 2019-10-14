f3804ee9725381b0d138ec47ae1f15722d4cf231
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-10-14 15:53:13 +1100

    Loadig config from file (Store) and passing it to edit-layout component

--- src/app/components/pages/news/news.component.ts
+++ src/app/components/pages/news/news.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit }  from '@angular/core';
 
+import { Store, select }      from '@ngrx/store';
+
+import { IAppState }          from './../../../store/state/app.state';
+import { GetConfig }          from './../../../store/actions/config.actions';
+import { selectConfigObject } from './../../../store/selectors/config.selectors';
+
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
+  config$ = this._store.pipe(select(selectConfigObject));
 
-  constructor() { }
+  constructor(private _store: Store<IAppState>) { }
 
   ngOnInit() {
+    this._store.dispatch(new GetConfig());
   }
 }
