36ce8d7edeab3e46dbef43b3328f610555060275
Prasada Indukuri <contactipraju@gmail.com>
2021-03-10 13:40:16 +1100

    fix for deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,14 @@ git fetch -p
 git pull
 git checkout gh-pages
 git merge origin/master
-#ng build --prod --base-href https://contactipraju.github.io/telescope/
-#sudo ngh --dir=dist/telescope
-ng deploy --base-href=/telescope/
+
+ng build --prod --output-path dist/telescope --base-href="https://contactipraju.github.io/telescope/"
+sudo ngh --dir=dist/telescope
+
+#ng deploy --base-href=/telescope/ --no-silent
+
+#ng build --prod --base-href=/telescope/
+#npx angular-cli-ghpages
+
 git checkout master
 git pull
